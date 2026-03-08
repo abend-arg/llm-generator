4a1f61c25650fb27b9540ca5df9a498b14842d95
Marcos Hernan Garcia <marcos.h.garcia@gmail.com>
2026-03-08 00:36:58 -0300

    feat(application): Introduce ExportContentService

--- apps/api/application/__init__.py
+++ apps/api/application/__init__.py
@@ -1,3 +1,7 @@
 from .ping_service import PingService
+from .export_content_service import ExportContentService
 
-__all__ = ["PingService"]
+__all__ = [
+    "ExportContentService",
+    "PingService",
+]
---
+++ apps/api/application/export_content_service.py
@@ -0,0 +1,4 @@
+class ExportContentService:
+    def export(self, url: str) -> tuple[str, str]:
+        _ = url
+        return "llms.txt", ""
